52b17fb14896fd6ce876b038fbde1e112b67ad8d
Edgar Mateus Ruiz Garcia <EdgarMateus@live.com>
2021-08-31 19:45:54 -0700

    Tarea

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Nombre, Edad;
+            Console.WriteLine("Escribe tu nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad");
+            Edad = Console.ReadLine();
+            Console.WriteLine("Te llamas " + Nombre + " Y tienes " + Edad + " años");
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
