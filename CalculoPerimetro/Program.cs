78e027c97d6d7c7bbe02842ab2bc405211045a59
Edgar Mateus Ruiz Garcia <EdgarMateus@live.com>
2021-08-31 19:16:16 -0700

    Agregar archivos de proyecto.

---
+++ CalculoPerimetro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 10;
+            int c = 7;
+            int p = a + b + c;
+            Console.WriteLine("El perimetro del triangulo es:" + p);
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ejercicio_String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_String
+{
+    class Program
+    {
+        //Pedir por consola el nombre de una persona 
+
+        static void Main(string[] args)
+        {
+            string Nombre, Ciudad;
+            Console.WriteLine("Escribe tu Nombre");
+            Nombre = Console.ReadLine();
+            Console.WriteLine("Escribe una Ciudad");
+            Ciudad = Console.ReadLine();
+            Console.WriteLine("Hola " + Nombre + " Bienvenido a " + Ciudad);
+            Console.WriteLine("Presiona cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
